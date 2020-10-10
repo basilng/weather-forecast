8f4e4d91beb1f87c80e39fd1ffb281f0534fa833
Basil N G <basilng.bng@gmail.com>
2020-10-10 14:33:27 +0530

    Updating State

--- src/index.js
+++ src/index.js
@@ -4,19 +4,27 @@ import ReactDOM from "react-dom";
 class App extends React.Component {
   constructor(props) {
     super(props);
-
+    //this is the only time we do direct assignment for state
     this.state = {
       lat: null,
     };
+    //render method may call several time in a component so
+    //we don't want to slow down it by doing some complex operations
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        //to update state we called setState
+        this.setState({ lat: position.coords.latitude });
+
+        //we can;t do ....
+        //this.state.lat = position.coords.latitude
+      },
+      (err) => console.log(err)
+    );
   }
 
   //React says we must have to define render()
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
-    return <div>Lattitude: </div>;
+    return <div>Lattitude: {this.state.lat}</div>;
   }
 }
 
